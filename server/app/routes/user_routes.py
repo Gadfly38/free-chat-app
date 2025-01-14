3cc408e4f159eb3b0cda78f68f72607c077538df
CodeCastle0713 <true.dragon.p@gmail.com>
2025-01-15 02:19:03 +0900

    updated : google sign in

--- server/app/routes/user_routes.py
+++ server/app/routes/user_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 
-from app.controllers.user_controller import sign_up, sign_in
-from app.models.user_model import UserSignUpModel, UserSignInModel
+from app.controllers.user_controller import sign_up, sign_in, google_sign_in
+from app.models.user_model import UserSignUpModel, UserSignInModel, TokenModel
 
 router = APIRouter()
 
@@ -12,3 +12,7 @@ async def create_user(user:UserSignUpModel):
 @router.post("/api/auth/signin")
 async def insert_user(user:UserSignInModel):
     return await sign_in(user)
+
+@router.post("/api/auth/google")
+async def check_token(token:TokenModel):
+    return await google_sign_in(token)
