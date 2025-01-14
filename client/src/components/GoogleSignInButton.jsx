91401c96b1a6738b8f04e827e0f4718c288c2c87
CodeCastle0713 <true.dragon.p@gmail.com>
2025-01-15 05:09:21 +0900

    fixed : google signin button

--- client/src/components/GoogleSignInButton.jsx
+++ client/src/components/GoogleSignInButton.jsx
@@ -1,28 +1,40 @@
 // src/components/GoogleSignInButton.jsx
 import React from "react";
-import { useGoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 import api from "@/api/axios";
 
 const GoogleSignInButton = ({ content, color }) => {
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      console.log("Google OAuth Success:", tokenResponse.access_token);
-      api.post("/auth/google", { token: tokenResponse.access_token });
-    },
-  });
+  const handleSuccess = async (credentialResponse) => {
+    console.log("Google OAuth Success:", credentialResponse);
+    const token = credentialResponse.credential;
+    console.log("idToken_-_---", token);
+    // Send the ID token to your backend for verification
+    const response = await api.post("/auth/google", { token });
+    console.log("response_-_---", response);
+  };
+
+  const handleError = () => {
+    console.error("Login Failed");
+  };
 
   return (
-    <button
-      className={`flex items-center justify-center bg-${color}-500 hover:bg-${color}-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-${color}-400 focus:ring-opacity-50 disabled:opacity-50`}
-      onClick={login}
-    >
-      <img
-        src="/images/google.png"
-        alt="Google icon"
-        className="w-5 h-5 mr-2"
-      />
-      {content}
-    </button>
+    <GoogleLogin
+      onSuccess={handleSuccess}
+      onError={handleError}
+      render={({ onClick }) => (
+        <button
+          className={`flex items-center justify-center bg-${color}-500 hover:bg-${color}-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-${color}-400 focus:ring-opacity-50 disabled:opacity-50`}
+          onClick={onClick}
+        >
+          <img
+            src="/images/google.png"
+            alt="Google icon"
+            className="w-5 h-5 mr-2"
+          />
+          {content}
+        </button>
+      )}
+    />
   );
 };
 
